cccbb092f41fd86e584af64c416d812521e0c89e
RicharDev01 <pinedaricardo587@gmail.com>
2026-04-20 16:53:29 -0600

    Arquitectura Limpia (Clean Architecture) en Capas Basado en el dominio

--- src/main/java/tienda/milagro/sistemafacturacion/web/controladores/HolaControllador.java
+++ src/main/java/tienda/milagro/sistemafacturacion/web/controladores/HolaControllador.java
@@ -1,4 +1,4 @@
-package tienda.milagro.sitemafacturacion;
+package tienda.milagro.sistemafacturacion.web.controladores;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
